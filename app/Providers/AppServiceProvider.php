e904d593fdf7ca970eeb2c56772b46867a9b7a61
raul farl <raulfarllobell@gmail.com>
2021-12-17 14:02:53 +0100

    create file procfile and changes the file app-serive-providers, we add 2 fragments of code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,18 +12,23 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+       if (env('REDIRECT_HTTPS')) {
+           $url->formatScheme('https://');
+       }
+       Schema::defaultStringLength(191);
     }
 }
